0b58b8119e5084de6c04a55814fb64c4b7b0cba7
Brandon Borders <brandon@camelcased.com>
2012-08-30 22:56:16 -0700

    Correct link format

--- application/views/templates/main.blade.php
+++ application/views/templates/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>@yield('title')</title>
   <meta name="viewport" content="width=device-width" />
   <link href="http://fonts.googleapis.com/css?family=Londrina+Solid|Montserrat" rel="stylesheet" type="text/css" />
@@ -18,8 +18,8 @@
     <nav>
       <ul>
         <li class="enders">{{ HTML::link_to_route('root', 'home')}}<div class="first"></div></li>
-        <li>{{ HTML::link_to_route('involved', '<p class="nav">get</p><p>involved</p>', array('encode' => false)) }}<div class="middle"></li>
-        <li>{{ HTML::link_to_route('about', '<p class="nav">about</p><p>us</p>', array('encode' => false)) }}<div class="last"></div></li>
+        <li>{{ HTML::link_to_route('involved', '<p class="nav">get</p><p>involved</p>', null, array('encode' => false)) }}<div class="middle"></li>
+        <li>{{ HTML::link_to_route('about', '<p class="nav">about</p><p>us</p>', null, array('encode' => false)) }}<div class="last"></div></li>
         <li class="enders">{{ HTML::link_to_route('pictures', 'pictures') }}</li>
       </ul>
     </nav>
